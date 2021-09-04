1bdf75974b74982ef6996d53a1bf5e1507059e7a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-04 11:47:08 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+// components
+import Home from './pages/Home';
+
 export default function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path='/' component={Home} />
       </Switch>
     </Router>
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from '../styled-components/home';
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>Sample</h1>
+    </Container>
+  )
+}
