aa9f8ecd28db30bf91d7dd6210189535f4b83eaa
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-04 12:50:22 +0530

    redirect bounce

--- src/pages/Specific.jsx
+++ src/pages/Specific.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { users } from '../utils/list.json'
 import { useParams } from 'react-router-dom';
 
 export default function Specific() {
+  const history = useHistory();
+
   const params = useParams();
   const userId = params.userId;
-
   const user = users[userId];
+
+  useEffect(() => {
+    if (!user) history.push('/');
+  }, [user, history])
 
   return (
     <div>
-      <h2>{user.name}</h2>
+      <h2>{user?.name}</h2>
     </div>
   )
 }
